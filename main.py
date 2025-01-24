6518bd92b563c886e6d2fe53cbd34e1e67f4614e
OG-Fishies <165400445+EggRecRoom@users.noreply.github.com>
2025-01-24 12:00:49 +0000

    remade main.py

--- main.py
+++ main.py
@@ -2,15 +2,10 @@ import colorama
 from colorama import Fore
 colorama.init()
 print(f"{Fore.MAGENTA}Starting")
-#import api, notify, Matchmaking
 import threading
 import ProgramUtils
 import os
 import data
-
-#threading.Thread(target=notify.run).start()
-#threading.Thread(target=Matchmaking.run).start()
-#api.run()
 
 if not os.path.exists(data.saveDataPath):
     import SetUp
@@ -18,22 +13,22 @@ if not os.path.exists(data.saveDataPath):
     SetUp.setUp()
 
 def mainMenu():
-    print(f"Rec.py2023 - Open source Old RecRoom server software for 2023. (Version:{ProgramUtils.getVersion()})\nMade and provided by EggRecRoom\nDownload source code here: null")
+    print(f"Rec.py2023 - Open source old RecRoom server for 2023. (Version:{ProgramUtils.getVersion()})\nMade by EggRecRoom")
     print(f"\n{Fore.RED}! Some Rooms Wont Load !{Fore.MAGENTA}")
-    print(f"\n\n(1) Change Settings\n(2) Modify Profile\n(3) Start Server")
+    print(f"\n\n1. Change Settings\n2. Modify Profile\n3. Start Server")
     ufgtftftfty = input()
     _input = int(ufgtftftfty)
-    if _input == 1:
-        pass
-    elif _input == 2:
-        pass
-    elif _input == 3:
-        ProgramUtils.clearScreen()
-        print("Starting")
-        import api, notify, Matchmaking
-        threading.Thread(target=notify.run).start()
-        threading.Thread(target=Matchmaking.run).start()
-        api.run()
+    match _input:
+        case 3:
+            ProgramUtils.clearScreen()
+            print("Starting")
+            import api, notify, Matchmaking
+            threading.Thread(target=notify.run).start()
+            threading.Thread(target=Matchmaking.run).start()
+            api.run()
+        case _:
+            ProgramUtils.clearScreen()
+            mainMenu()
 
 
 ProgramUtils.clearScreen()
